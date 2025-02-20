cffd24a7f870d1fc6112f455e40ed6480ad4592a
Kirill Andreev <hizenberg228@mail.ru>
2025-02-20 22:20:11 +0300

    Added main menu

---
+++ handlers/keyboards/menu.py
@@ -0,0 +1,23 @@
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from handlers.buttons import menu as btn
+
+
+def main_menu_keyboard(admin: bool) -> InlineKeyboardBuilder:
+    """Клавиатура главного меню"""
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.row(
+        InlineKeyboardButton(text=f"{btn.PROFILE}", callback_data="menu|profile"),
+        InlineKeyboardButton(text=f"{btn.BUY}", callback_data="menu|buy")
+    )
+    keyboard.row(
+        InlineKeyboardButton(text=f"{btn.BALANCE}", callback_data="menu|balance"),
+        InlineKeyboardButton(text=f"{btn.KEYS}", callback_data="menu|keys")
+    )
+
+    if admin:
+        keyboard.row(InlineKeyboardButton(text=f"{btn.ADMIN}", callback_data="menu|admin"))
+
+    return keyboard
--- handlers/users.py
+++ handlers/users.py
@@ -1,8 +1,11 @@
 from typing import Any
 
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import Command
+
 from database.orm import AsyncOrm
+from handlers.buttons import commands as cmd
+from handlers.keyboards import menu as menu_kb
 
 router = Router()
 
@@ -15,3 +18,15 @@ async def start_handler(message: types.Message) -> None:
 @router.message(Command("help"))
 async def start_handler(message: types.Message) -> None:
     await message.answer("Help message")
+
+
+@router.message(Command(f"{cmd.MENU[0]}"))
+@router.callback_query(F.data == "main_menu")
+async def main_menu(message: types.Message | types.CallbackQuery, admin: bool) -> None:
+    """Отправка приветственного сообщения"""
+    name: str = message.from_user.first_name if message.from_user.first_name else message.from_user.username
+
+    msg = f"Рады видеть тебя, <b>{name}</b>!\n\n" \
+          f"Пополняйте баланс и оформляйте подписку для доступа к VPN"
+    await message.answer(msg, reply_markup=menu_kb.main_menu_keyboard(admin).as_markup())
+
