edcbd3c45a8a175d490318106e18932cee74412b
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2025-04-06 15:44:51 +0300

    Add instruction handler

--- handlers/users.py
+++ handlers/users.py
@@ -1,12 +1,11 @@
-
-
 from aiogram import Router, types, F
 from aiogram.filters import Command
-from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 
 from handlers.buttons import commands as cmd
 from handlers.keyboards import menu as menu_kb
+from handlers.messages.users import instruction_message
+from handlers.keyboards import users as kb
 
 
 router = Router()
@@ -14,8 +13,15 @@ router = Router()
 
 @router.message(Command(f"{cmd.INSTRUCTION[0]}"))
 async def instruction_handler(message: types.Message) -> None:
-    # TODO не потерять (копирование текста по нажатию)
-    await message.answer("`Instruction message`", parse_mode=ParseMode.MARKDOWN_V2)
+    """Выбор ОС для инструкции"""
+    msg = instruction_message()
+    await message.answer(msg, reply_markup=kb.choose_os().as_markup())
+
+
+@router.callback_query(F.data.split("|")[0] == "instruction")
+async def instruction_for_os(callback: types.CallbackQuery) -> None:
+    """Инструкции для отдельных ОС"""
+    pass
 
 
 @router.message(Command(f"{cmd.HELP[0]}"))
