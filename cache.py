98f9a64c77b52bf8a4ddc8a0fb388f12801088e8
Kirill Andreev <hizenberg228@mail.ru>
2025-04-21 22:49:26 +0300

    asd

--- cache.py
+++ cache.py
@@ -3,13 +3,14 @@ import redis
 from settings import settings
 from logger import logger
 
-r = redis.Redis(host='redis', port=settings.redis.redis_port, password=settings.redis.redis_password)
-
-try:
-    response = r.ping()
-    if response:
-        logger.info("Successful connection to Redis.")
-    else:
-        logger.error("Не удалось подключиться к Redis.")
-except redis.exceptions.RedisError as e:
-    logger.error(f"Ошибка подключения к Redis: {e}")
+# r = redis.Redis(host='redis', port=settings.redis.redis_port, password=settings.redis.redis_password)
+r = redis.Redis()
+#
+# try:
+#     response = r.ping()
+#     if response:
+#         logger.info("Successful connection to Redis.")
+#     else:
+#         logger.error("Не удалось подключиться к Redis.")
+# except redis.exceptions.RedisError as e:
+#     logger.error(f"Ошибка подключения к Redis: {e}")
