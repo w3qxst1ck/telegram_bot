7167a1e7d8cf3a3674e910b086fa136e8d4eec07
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2025-04-23 21:40:42 +0300

    Add error logger, fix balance confirm digit

--- logger.py
+++ logger.py
@@ -20,9 +20,21 @@ logger.add(
 
 # запись в .log файл
 log_file_path = os.path.join(log_folder, "bot.log")
+
+# запись всех логов
 logger.add(
     log_file_path,
     level="INFO",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | {message}",
+    rotation="100 MB",
+    retention="60 days",
+)
+
+# запись ошибок
+error_log_file_path = os.path.join(log_folder, "bot_errors.log")
+logger.add(
+    error_log_file_path,
+    level="ERROR",
     format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | {message}",
     rotation="100 MB",
     retention="60 days",
